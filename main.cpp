2461d0364dc8337ca7cdd6e1737b7c815826c2ec
Adrian <adrian@mxlinux.org>
2021-01-31 20:00:58 -0500

    added help text

--- main.cpp
+++ main.cpp
@@ -9,10 +9,21 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QCommandLineParser parser;
-    parser.setApplicationDescription(QObject::tr("Pop-up with exit options for MX Fluxbox"));
+    parser.setApplicationDescription("Pop-up with exit options for MX Fluxbox");
     parser.addOption({{"v", "vertical"}, "Display buttons in a vertical window"});
     parser.addOption({{"h", "horizontal"}, "Display buttons in a horizontal window"});
+    parser.addOption({"help", "use -v, --vertical to display buttons vertically\nuse -h, --horizontal to display buttons horizontally."});
     parser.process(a);
+    if (parser.isSet("help")) {
+        qDebug("Pop-up with exit options for MX Fluxbox");
+        qDebug("Usage: exit-options [options]\n");
+        qDebug("Options:");
+        qDebug("  -v, --vertical\t Option to display buttons vertically");
+        qDebug("  -h, --horizontal\t Option to display buttons horizontally\n");
+        qDebug("The last option used will be remembered and used the next time you start the app");
+        qDebug("Alternativelly, set the option 'layout=horizontal' or 'layout=vertical' in ~/.config/MX-Linux/exit-options.conf");
+        exit(EXIT_SUCCESS);
+    }
 
     MainWindow w(parser);
     w.show();
