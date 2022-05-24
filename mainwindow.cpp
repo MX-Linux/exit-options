d3c45e3c86c6c57f968803bc2f0fd27186b10135
Adrian <adrian@mxlinux.org>
2022-05-24 01:41:09 -0400

    Update C++ standard to c++17; various style edits

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,22 +7,21 @@
 MainWindow::MainWindow(const QCommandLineParser &arg_parser, QWidget *parent) :
     QDialog(parent)
 {
-    QPushButton *buttonLock = new QPushButton(QIcon("/usr/local/share/icons/system-lock-mxfb.png"), QString());
-    QPushButton *buttonExit = new QPushButton(QIcon("/usr/local/share/icons/system-log-out.png"), QString());
-    QPushButton *buttonSleep = new QPushButton(QIcon("/usr/local/share/icons/system-sleep.png"), QString());
-    QPushButton *buttonRestart = new QPushButton(QIcon("/usr/local/share/icons/system-restart.png"), QString());
-    QPushButton *buttonShutdown = new QPushButton(QIcon("/usr/local/share/icons/system-shutdown.png"), QString());
+    auto pushLock = new QPushButton(QIcon("/usr/local/share/icons/system-lock-mxfb.png"), QString());
+    auto pushExit = new QPushButton(QIcon("/usr/local/share/icons/system-log-out.png"), QString());
+    auto pushSleep = new QPushButton(QIcon("/usr/local/share/icons/system-sleep.png"), QString());
+    auto pushRestart = new QPushButton(QIcon("/usr/local/share/icons/system-restart.png"), QString());
+    auto pushShutdown = new QPushButton(QIcon("/usr/local/share/icons/system-shutdown.png"), QString());
 
-    connect(buttonLock, &QPushButton::clicked, this, &MainWindow::on_buttonLock);
-    connect(buttonExit, &QPushButton::clicked, this, &MainWindow::on_buttonExit);
-    connect(buttonSleep, &QPushButton::clicked, this, &MainWindow::on_buttonSleep);
-    connect(buttonRestart, &QPushButton::clicked, this, &MainWindow::on_buttonRestart);
-    connect(buttonShutdown, &QPushButton::clicked, this, &MainWindow::on_buttonShutdown);
-    QList<QPushButton *> btnList {buttonLock, buttonExit, buttonSleep, buttonRestart, buttonShutdown};
+    connect(pushLock, &QPushButton::clicked, this, &MainWindow::on_pushLock);
+    connect(pushExit, &QPushButton::clicked, this, &MainWindow::on_pushExit);
+    connect(pushSleep, &QPushButton::clicked, this, &MainWindow::on_pushSleep);
+    connect(pushRestart, &QPushButton::clicked, this, &MainWindow::on_pushRestart);
+    connect(pushShutdown, &QPushButton::clicked, this, &MainWindow::on_pushShutdown);
+    QList<QPushButton*> btnList {pushLock, pushExit, pushSleep, pushRestart, pushShutdown};
 
     QBoxLayout *layout;
-    if ((settings.value("layout").toString() == "horizontal"
-         || arg_parser.isSet("horizontal"))
+    if ((settings.value("layout").toString() == "horizontal" || arg_parser.isSet("horizontal"))
             && !arg_parser.isSet("vertical")) {
         horizontal = true;
         layout = new QHBoxLayout(this);
@@ -31,12 +30,12 @@ MainWindow::MainWindow(const QCommandLineParser &arg_parser, QWidget *parent) :
         layout = new QVBoxLayout(this);
     }
 
-    layout->addWidget(buttonLock);
-    layout->addWidget(buttonExit);
+    layout->addWidget(pushLock);
+    layout->addWidget(pushExit);
     if (!isRaspberryPi())
-        layout->addWidget(buttonSleep);
-    layout->addWidget(buttonRestart);
-    layout->addWidget(buttonShutdown);
+        layout->addWidget(pushSleep);
+    layout->addWidget(pushRestart);
+    layout->addWidget(pushShutdown);
     setLayout(layout);
 
     for (auto btn : btnList) {
@@ -53,9 +52,8 @@ MainWindow::MainWindow(const QCommandLineParser &arg_parser, QWidget *parent) :
        QByteArray geometry = saveGeometry();
        restoreGeometry(settings.value("geometry").toByteArray());
        // if too wide/tall reset geometry
-       if ((horizontal && size().height() >= size().width() )
-               || (!horizontal && size().height() <= size().width()))
-                   restoreGeometry(geometry);
+       if ((horizontal && size().height() >= size().width()) || (!horizontal && size().height() <= size().width()))
+           restoreGeometry(geometry);
     }
 }
 
@@ -63,17 +61,17 @@ MainWindow::~MainWindow()
 {
 }
 
-void MainWindow::on_buttonLock()
+void MainWindow::on_pushLock()
 {
     system("dm-tool switch-to-greeter &");
 }
 
-void MainWindow::on_buttonExit()
+void MainWindow::on_pushExit()
 {
     system("fluxbox-remote 'Exit' || killall fluxbox &");
 }
 
-void MainWindow::on_buttonSleep()
+void MainWindow::on_pushSleep()
 {
     system("sudo pm-suspend &");
 }
@@ -81,18 +79,15 @@ void MainWindow::on_buttonSleep()
 void MainWindow::saveSettings()
 {
     settings.setValue("geometry", saveGeometry());
-    if (horizontal)
-        settings.setValue("layout", "horizontal");
-    else
-        settings.setValue("layout", "vertical");
+    settings.setValue("layout", horizontal ? "horizontal" : "vertical");
 }
 
-void MainWindow::on_buttonRestart()
+void MainWindow::on_pushRestart()
 {
     system("sudo reboot &");
 }
 
-void MainWindow::on_buttonShutdown()
+void MainWindow::on_pushShutdown()
 {
     system("sudo /sbin/halt &");
 }
