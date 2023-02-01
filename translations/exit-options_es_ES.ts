8a34a43dea6f3f933358cae80fec56d9cc3f8b9f
Adrian <adrian@mxlinux.org>
2023-02-01 16:18:57 -0500

    Add new languages

---
+++ translations/exit-options_es_ES.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="16"/>
+        <source>Lock Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="17"/>
+        <source>Log Out</source>
+        <translation>Cerrar sesión</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="18"/>
+        <source>Suspend</source>
+        <translation>Suspender</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="19"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="20"/>
+        <source>Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation type="vanished">Reiniciar</translation>
+    </message>
+</context>
+</TS>
