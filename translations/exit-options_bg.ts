6700d7d7c0b1a8fd685ec140084e117a1a207bf2
Adrian <adrian@mxlinux.org>
2023-02-01 17:55:40 -0500

    Add the new strings to translate

--- translations/exit-options_bg.ts
+++ translations/exit-options_bg.ts
@@ -29,4 +29,71 @@
         <translation>Изключване</translation>
     </message>
 </context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="33"/>
+        <location filename="../main.cpp" line="41"/>
+        <source>Pop-up with exit options for MX Fluxbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="34"/>
+        <source>Display buttons in a vertical window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="35"/>
+        <source>Display buttons in a horizontal window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="36"/>
+        <source>Timeout duration in seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="37"/>
+        <source>use -v, --vertical to display buttons vertically
+use -h, --horizontal to display buttons horizontally.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="42"/>
+        <source>Usage: exit-options [options]
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="43"/>
+        <source>Options:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="44"/>
+        <source>  -h, --horizontal	 Option to display buttons horizontally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="45"/>
+        <source>  -v, --vertical	 Option to display buttons vertically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="46"/>
+        <source>  -t, --timeout &lt;sec&gt;	 Timeout duration in seconds
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="47"/>
+        <source>The display orientation option used will be remembered and used the next time you start the app</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="49"/>
+        <source>Alternativelly, set the option &apos;layout=horizontal&apos; or &apos;layout=vertical&apos; in ~/.config/MX-Linux/exit-options.conf</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
