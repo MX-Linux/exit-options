15aea38c137075c9c388ff07c19448754af7d470
Adrian <adrian@mxlinux.org>
2025-08-07 15:00:43 -0400

    Set Qt platform to XCB if not set

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ using namespace std::chrono_literals;
 
 int main(int argc, char *argv[])
 {
+    // Set Qt platform to XCB (X11) if not already set and we're in X11 environment
+    if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        if (!qEnvironmentVariableIsEmpty("DISPLAY")) {
+            qputenv("QT_QPA_PLATFORM", "xcb");
+        }
+    }
+
     QApplication app(argc, argv);
     QApplication::setOrganizationName("MX-Linux");
 
